9fe3847dbac4f8cdf68788635510214078c56552
zhenxxx7 <jordy.zeventh@gmail.com>
2022-06-29 11:45:18 +0700

    update code

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'ae4e9e8760fdca379ae92b3bc7b47a61ADFDFJS',
+    apiKey: 'ae4e9e8760fdca379ae92b3bc7b47a61',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
